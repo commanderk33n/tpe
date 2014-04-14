d698797b7d9246715e8b0acf5148ac71f10b92b0
Phil0110 <philsieb01@gmail.com>
2014-04-14 13:22:06 +0100

    wesen

--- tpe/src/imb/SS14/tpegr01/racewars/Wesen.java
+++ tpe/src/imb/SS14/tpegr01/racewars/Wesen.java
@@ -1,5 +1,38 @@
 package imb.SS14.tpegr01.racewars;
 
-public class Wesen {
-// hallo
+public abstract class Wesen {
+
+	private String rasse;
+	private int kosten;
+	private double lebenspunkte;
+	private double ruestung;
+	private double schaden;
+	private double geschwindigkeit;
+	private double spezialattribut;
+
+	public Wesen(String rasse, int kosten, double lebenspunkte,
+			double ruestung, double schaden, double geschwindigkeit,
+			double spezialattribut) {
+		this.rasse = rasse;
+		this.kosten = kosten;
+		this.lebenspunkte = lebenspunkte;
+		this.geschwindigkeit = geschwindigkeit;
+		this.ruestung = ruestung;
+		this.schaden = schaden;
+		this.spezialattribut = spezialattribut;
+	}
+
+	public boolean isLebendig() {
+		if (this.lebenspunkte > 0) {
+			return true;
+		} else {
+			return false; // todo remove from squad
+		}
+	}
+
+	public String toString() {
+		String details = "";
+		return details;
+	}
+
 }
