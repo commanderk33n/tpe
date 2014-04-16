ac6953ebf074f008e8a7296f4a39705c8d47b4ec
hanfaplast <hanfaplast@googlemail.com>
2014-04-16 19:18:00 +0200

    attack Geist hinzugefügt

--- tpe/src/imb/SS14/tpegr01/racewars/wesen/Geist.java
+++ tpe/src/imb/SS14/tpegr01/racewars/wesen/Geist.java
@@ -1,9 +1,15 @@
 package imb.SS14.tpegr01.racewars.wesen;
 
-public class Geist {    // einzigstes Attribut = schaden 1  ??
+import imb.SS14.tpegr01.racewars.Kaempfer;
+
+public class Geist implements Kaempfer { 
 
 	public Geist() {
 
+	}
+
+	public double attacke(Kaempfer r) {
+		return 1;
 	}
 
 }
