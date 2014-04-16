e4dd8af4a6d089ac8f8c86a7d453439b7eb9964c
hanfaplast <hanfaplast@googlemail.com>
2014-04-16 14:58:29 +0200

    beschraenkeSchaden methode in Klasse Mensch hinzugefügt

--- tpe/src/imb/SS14/tpegr01/racewars/wesen/Mensch.java
+++ tpe/src/imb/SS14/tpegr01/racewars/wesen/Mensch.java
@@ -10,6 +10,8 @@ public class Mensch extends Wesen {
 		super("Mensch", istHeld, kosten, lebenspunkte, 0.4, 40.0, 2.0, 10.0);
 	}
 	
-	// beschränkeSchaden methode  toDO
+	protected double beschraenkeSchaden(double damage) { 
+	    return 0; 
+	    } 
 
 }
