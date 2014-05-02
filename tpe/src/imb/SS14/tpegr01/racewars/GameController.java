44af817605b55d2fc6b5ec154ced9d8c80f36006
Tim Hubert <timhubert@hotmail.de>
2014-05-02 16:35:17 +0200

    javadoc für GameController

--- tpe/src/imb/SS14/tpegr01/racewars/GameController.java
+++ tpe/src/imb/SS14/tpegr01/racewars/GameController.java
@@ -1,11 +1,37 @@
 package imb.SS14.tpegr01.racewars;
 
+/**
+ * Racewars ist ein Spiel, in dem zwei Kontrahenten maximal 2 aus 4 Rassen
+ * wählen können, um Elfendollar (auch ungleiche Gewichtung) in die gewählten
+ * Rassen zu investieren, dadurch entsteht ein Squad pro Spieler.
+ * Die Armee aus maximal zwei Rassen kämpfen im Folgenden gegeneinander.
+ * Der Spieler gewinnt, der den gegnerischen Squad vernichtend schlägt.
+ * 
+ * Die Klasse <code>GameController</code> erstellt zwei Spieler und startet
+ * das Spiel mit diesen.
+ * 
+ * 
+ * @author Tim Hubert
+ * @author Torsten Müller
+ * @author Philipp Siebert
+ * @version 2 28/04/2014
+ *
+ */
+
 public class GameController {
 
 	private int runde;
 	private Squad spieler1;
 	private Squad spieler2;
 	private boolean istSpieler1AnDerReihe;
+	
+	/**
+	 * Der Konstruktor <code>GameController</code> erstellt den zwei Spielern
+	 * zwei Squads.
+	 * 
+	 * @param spieler1  Erster Spieler
+	 * @param spieler2  Zweiter Spieler
+	 */
 
 	public GameController(Spieler spieler1, Spieler spieler2) {
 		this.runde = 0;
@@ -14,6 +40,12 @@ public class GameController {
 		istSpieler1AnDerReihe = true;
 	}
 
+	/**
+	 * Startet das Spiel. Die zwei Spieler attackieren sich in Runden.
+	 * Das Abbild der Kampfhandlung wird in der Klasse <code>GameViewer</code>
+	 * dargestellt.
+	 */
+	
 	public void runGame() {
 		GameViewer.printGame(this);
 		while (spieler1.lebendigeWesen > 0 && spieler2.lebendigeWesen > 0) {
@@ -30,9 +62,18 @@ public class GameController {
 		GameViewer.printResult(this);
 	}
 
+	/**
+	 * 
+	 * @return  Gibt die aktuelle Spielrunde zurück.
+	 */
 	public int getRunde() {
 		return this.runde;
 	}
+	
+	/**
+	 * Erstellt die Spieler mit ihrem Squadnamen, den gewünschten Rassen und
+	 * deren Investition. Und erstellt daraufhin ein Spiel mit zweier Spieler.
+	 */
 
 	public static void main(String[] args) {
 		Spieler spieler1 = new Spieler("The walking Dead", "untote", 4224);
@@ -46,6 +87,11 @@ public class GameController {
 		gameTWO.runGame();
 
 	}
+	
+	/**
+	 * 
+	 * @return  Gibt die Squads der zwei Spieler zurück.
+	 */
 
 	public Squad[] getSquads() {
 		return new Squad[] { this.spieler1, this.spieler2 };
