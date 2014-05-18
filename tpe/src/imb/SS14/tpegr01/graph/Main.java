b63fb97d858f1c556a34c39c9af6301d691ad91e
hanfaplast <hanfaplast@googlemail.com>
2014-05-19 00:10:44 +0200

    irgendwas ist nun in der deepSearch faul - er findet es erste element nicht :(

--- tpe/src/imb/SS14/tpegr01/graph/Main.java
+++ tpe/src/imb/SS14/tpegr01/graph/Main.java
@@ -7,24 +7,33 @@ public class Main {
 		first.addChild(new Node<String>("second", "Stirb!"));
 		first.addChild(new Node<String>("third", "Neeeiiiiinnn!"));
 		first.addChild(new Node<String>("fourth", "Doch!"));
-		Graph graph1 = new Graph(first);
+		Graph<String> graph1 = new Graph<String>(first);
 		System.out.println(graph1.getFirst().getChildren().get(1));
-		Node<Integer> newNode=new Node<Integer>("zahl1", 7);
+		Node<Integer> newNode = new Node<Integer>("zahl1", 7);
 		graph1.getFirst().getChildren().get(0).addChild(first);
 		graph1.getFirst().getChildren().get(1).addChild(newNode);
-		System.out.println(graph1.getFirst().getChildren().get(0).getChildren());
+		System.out
+				.println(graph1.getFirst().getChildren().get(0).getChildren());
 		System.out.println(graph1.getFirst().getValue());
-		NodeList liste1=new NodeListImpl();
+		NodeList liste1 = new NodeListImpl();
 		graph1.copyInto(liste1);
 		System.out.println(graph1);
 		System.out.println(liste1);
+		System.out.println(graph1.getFirst());
 		BreadthFirstSearch<String> breadthFirstSearch = new BreadthFirstSearch<String>();
-		System.out.println(breadthFirstSearch.search("Doch!", first));
+		System.out
+				.println(breadthFirstSearch.search("Doch!", graph1.getFirst()));
+		System.out.println("Weg Breitensuche:" + breadthFirstSearch.getPath());
 		DeepSearch<String> deepSearch = new DeepSearch<String>();
-		System.out.println(deepSearch.search("Neeeiiiiinnn!", first));
+		System.out
+				.println(deepSearch.search("Neeeiiiiinnn!", graph1.getFirst()));
+		System.out.println("Weg Tiefensuche:" + deepSearch.getPath());
+		System.out.println(deepSearch.search("Stirb!", graph1.getFirst()));
+		System.out.println(breadthFirstSearch.search("Hallo?",
+				graph1.getFirst()));
+		System.out.println(deepSearch.search("Hallo?", graph1.getFirst()));  // BUGGGGYYYYY
+		System.out.println("Weg Tiefensuche:" + deepSearch.getPath());
 		
-		
-
 	}
 
 }
