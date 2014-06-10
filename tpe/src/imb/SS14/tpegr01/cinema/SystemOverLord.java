b6eb7ea18a692228b620d03dea7f1cb2d8a7ef2b
p.siebert <p.siebert@141.19.145.24>
2014-06-10 09:26:35 +0200

    kleine überarbeitung und neue klassen hinzufgefügt

--- tpe/src/imb/SS14/tpegr01/cinema/SystemOverLord.java
+++ tpe/src/imb/SS14/tpegr01/cinema/SystemOverLord.java
@@ -5,4 +5,63 @@ import java.util.ArrayList;
 public class SystemOverLord {
 	private ArrayList<Cinema> cinemas = new ArrayList<Cinema>();
 
+	/**
+	 * Auslesen aller Filme mit Ihren Anfangszeiten als Array sortiert nach der
+	 * Anfangszeit. Filme können doppelt vorkommen, wenn sie unterschiedliche
+	 * Anfangszeiten haben. (getAlleFilmeMitZeiten)
+	 * 
+	 * @param c
+	 *            Kino dessen Filme ausgelesen werden sollen
+	 * @return Filme mit Ihren Anfangszeiten
+	 */
+	public Film[] getAllMoviesWithTimes(Cinema c) {
+		//TODO
+		return null;
+	}
+
+	/**
+	 * Auslesen aller Filme in einem bestimmten Saal mit den entsprechenden
+	 * Anfangszeiten als Array. Die Filme sind nach der Startzeit sortiert.
+	 * (getFilmeFuerSaalMitZeiten)
+	 */
+	public Film[] getMoviesForHallWithTimes(Cinema c, Hall h) {
+		//TODO
+		return null;
+	}
+
+	/**
+	 * Auslesen aller Filme, die im Kino laufen als Array. Hierbei soll jeder
+	 * Film nur einmal enthalten sein, auch wenn er zu mehreren Zeiten und in
+	 * unterschiedliche Sälen läuft. Die Methode ist uberladen und erlaubt in
+	 * einer Variante die Angabe eines Sortierkriteriums als Enumeration. Wird
+	 * kein Kriterium angegeben, werden die Filme nach dem Namen sortiert.
+	 * (getAlleFilme)
+	 * 
+	 * @param c
+	 *            Kino dessen Filme ausgelesen werden sollen
+	 * @return alle Filme eines Kinos
+	 */
+	public Film[] getAllmovies(Cinema c) {
+		//TODO
+		return null;
+	}
+
+	/**
+	 * Auslesen aller Filme, die im Kino laufen als Array. Hierbei soll jeder
+	 * Film nur einmal enthalten sein, auch wenn er zu mehreren Zeiten und in
+	 * unterschiedliche Sälen läuft. Die Methode ist uberladen und erlaubt in
+	 * einer Variante die Angabe eines Sortierkriteriums als Enumeration. Wird
+	 * kein Kriterium angegeben, werden die Filme nach dem Namen sortiert.
+	 * (getAlleFilme)
+	 * 
+	 * @param c
+	 *            Kino dessen Filme ausgelesen werden sollen
+	 * @param s
+	 *            Angabe des anzuwendenen Sortierkriteriums
+	 * @return alle Filme eines Kinos
+	 */
+	public Film[] getAllmovies(Cinema c, SortingCriterion s) {
+		//TODO
+		return null;
+	}
 }
--- tpe/src/imb/SS14/tpegr01/cinema/Time.java
+++ tpe/src/imb/SS14/tpegr01/cinema/Time.java
@@ -11,24 +11,56 @@ public class Time {
 		int index = time.indexOf(":");
 		int hour = Integer.valueOf(time.substring(0, index));
 		int minute = Integer.valueOf(time.substring(index + 1));
-		if (hour > 23 && hour < 0) {
-			// todo exeption
+		try {
+			if (hour > 23 || hour < 0) {
+				throw new MyTimeException(
+						"Unzulässige Stunde wurde eingegeben!");
 
-		}
-		if (minute > 59 && minute < 0) {
-			// todo exeption
+			}
+			if (minute > 59 || minute < 0) {
+				throw new MyTimeException(
+						"Unzulässige Stunde wurde eingegeben!");
+			}
+			return hour * 60 + minute;
+		} catch (MyTimeException e) {
+			System.out.println("Fehler: " + e.getMessage());
 		}
 
-		return hour * 60 + minute;
+		return -1;
 	}
-	public int getTimeAsMinutes(){
+
+	public void setTime(String time) {
+		this.minutes = parse(time);
+	}
+
+	public int getTimeAsMinutes() {
 		return this.minutes;
+	}
+
+	public int getHoursOnly() {
+		return (int) (this.minutes / 60);
+	}
+
+	public int getMinutesOnly() {
+		return (this.minutes % 60);
 	}
 
 	public String toString() {
 		String result = "" + (int) (this.minutes / 60) + ":" + this.minutes
 				% 60;
-		return "";
+		return result;
+	}
+
+	public class MyTimeException extends Exception {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		public MyTimeException(String message) {
+			super(message);
+		}
 	}
 
 }
